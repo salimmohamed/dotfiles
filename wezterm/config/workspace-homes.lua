ec32a9cd1d54a759c82801ac33ee328addcefd2f
salimmohamed <mohamsal@oregonstate.edu>
2026-01-14 02:31:04 -0800

    feat: migrate from wezterm to ghostty + tmux

--- wezterm/config/workspace-homes.lua
+++
@@ -1,8 +0,0 @@
--- Workspace home directory mappings
--- These define the default directory for each workspace
-local workspace_homes = {
-	school = "/Users/sm/Learn",
-	dev = "/Users/sm/Dev",
-}
-
-return workspace_homes
